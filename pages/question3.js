3bb3b8ca6b3367afede04492edeb24fa174d7227
Jan <kupper.thorge@gmail.com>
2025-03-04 09:37:18 +0100

    q3 done

--- pages/question3.js
+++ pages/question3.js
@@ -7,11 +7,47 @@
 // det skrivas ut "klick på Button" på sidan.
 // När man klickar på ButtonWithChildren-knappen
 // ska det skrivas ut "klick på ButtonWithChildren".
+//
+// Create two components: Button and ButtonWithChildren.
+// Button should have a prop `title` that specifies the title of the button.
+// ButtonWithChildren should use children to display the text in the button.
+// Both components must have a prop `onClick` which is called when the button is clicked.
+// Use both components on the page. When clicking the Button button shall
+// "click Button" is printed on the page.
+// When clicking the ButtonWithChildren button
+// should it print "click on ButtonWithChildren".
+
+import { useState } from 'react';
 
 export default function Question3() {
+  const [print, setPrint] = useState('');
+
   return (
     <div>
       <h1>Question 3</h1>
+      <div className='flex gap-4'>
+        <Button onClick={() => setPrint('click on Button')} title={'Button'} />
+        <ButtonWithChildren onClick={() => setPrint('click on ButtonWithChildren')}>
+          ButtonWithChildren
+        </ButtonWithChildren>
+      </div>
+      <p>{print}</p>
     </div>
+  );
+}
+
+function Button({ title, onClick }) {
+  return (
+    <button className='border rounded p-2 hover:bg-slate-200' onClick={onClick}>
+      {title}
+    </button>
+  );
+}
+
+function ButtonWithChildren({ children, onClick }) {
+  return (
+    <button className='border rounded p-2 hover:bg-slate-200' onClick={onClick}>
+      {children}
+    </button>
   );
 }
