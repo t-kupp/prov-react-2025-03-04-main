69ebe7cae4e480ab85e26e96773df08024c3b41d
Jan <kupper.thorge@gmail.com>
2025-03-04 09:10:46 +0100

    q1 done

---
+++ pages/question1.js
@@ -0,0 +1,34 @@
+// 1. Popup (3p)
+//
+// Skapa en knapp "visa" på sidan. När man klickar på knappen ska en popup visas.
+// Popup:en visar rubriken "En popup" och en stängknapp i en ruta.
+// När man klickar på stängknappen ska popupen tas bort.
+//
+// Create a "view" button on the page. When the button is clicked, a popup should appear.
+// The popup displays the title "A popup" and a close button in a box.
+// When the close button is clicked, the popup should be removed.
+
+import { useState } from 'react';
+
+export default function Question1() {
+  const [showPopup, setShowPopup] = useState(false);
+
+  return (
+    <div>
+      {showPopup && (
+        <div className='h-screen w-screen backdrop-brightness-50 flex items-center justify-center'>
+          <div className='bg-white rounded p-4'>
+            <p>A popup</p>
+            <button className='border rounded p-2 hover:bg-slate-200' onClick={() => setShowPopup(false)}>
+              Close
+            </button>
+          </div>
+        </div>
+      )}
+      <h1>Question 1</h1>
+      <button className='border rounded p-2 hover:bg-slate-200' onClick={() => setShowPopup(true)}>
+        Show Popup
+      </button>
+    </div>
+  );
+}
